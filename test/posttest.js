f59eec20c47e8bf035115527396bce484181bd1a
Ayon Lee <i@hyurl.com>
2020-10-14 00:50:24 +0800

    fix test scripts

--- test/posttest.js
+++ test/posttest.js
@@ -1,7 +1,7 @@
 const { execSync } = require("child_process");
 
 if (process.platform === "win32") {
-    execSync("rm .\\test\\.build -r -fo");
+    execSync("rmdir /S /Q .\\test\\.build");
 } else {
     execSync("rm -rf ./test/.build");
 }
--- test/pretest.js
+++ test/pretest.js
@@ -1,8 +1,8 @@
 const { execSync } = require("child_process");
 
 if (process.platform === "win32") {
-    execSync("mkdir -p .\\test\\.build\\test");
-    execSync("cp -R .\\test\\json .\\test\\.build\\test");
+    execSync("mkdir .\\test\\.build\\test\\json");
+    execSync("copy .\\test\\json .\\test\\.build\\test\\json");
     execSync("npx tsc -p .\\test");
 } else {
     execSync("mkdir -p ./test/.build/test");
